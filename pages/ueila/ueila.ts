7a53c4e30a85c67ae67c09d960173e36b2bc95aa
ebelland <enrico.bellandi@gmail.com>
2020-01-06 16:31:10 +0100

    2 files modified, 2 added

---
+++ pages/ueila/ueila.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-ueila',
+  templateUrl: 'ueila.html'
+})
+export class UeilaPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
